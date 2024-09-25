4ec4e0bf9603d0d0d75fe49ccee6142ed5c9751b
NicolasRodriguesdoNascimento <nicolasrodrigues@ufpr.br>
2024-09-25 19:13:36 -0300

    adicionado plot

--- Código.R
+++ Código.R
@@ -13,3 +13,17 @@ a >= b
 
 a = complex(real = 2, imaginary = 3)
 
+a <- runif(10, min = 0, max = 1)
+help(barplot)
+
+a <- 1:100
+
+b <- sqrt(a)
+
+plot(a,b, las = 1, main = "f(x) = √x", xlab = "x", ylab = "y")
+help(plot)
+
+
+paste("a","b","c","d","e","f", sep = "")
+
+
